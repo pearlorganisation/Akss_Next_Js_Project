044420b90988acd6d1c5f19fe7d1eee72d3145e5
Ranjan <ranjan@pearlorganisation.com>
2025-04-01 11:28:14 +0530

    added logo of the company

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,22 @@
 
 import Link from 'next/link';
 import React from 'react'
+import Image from 'next/image'
 
 import { SiteDrawer } from './SiteDrawer';
-
+import logoimage from "../../public/logo.png"
 const Header = () => {
   return (
     <header className="w-full ">
       <div className="bg-[#002171] text-white flex items-center justify-between py-4 px-16 rounded-b-3xl">
-        <Link href="/" className="text-lg font-medium">
-          iona.ai
+           <Link href={`/`}>
+           <Image
+           src={logoimage}
+           alt="logo"
+           width={100}
+           height={100}
+           className='w-full h-full'
+           />
         </Link>
        
 
